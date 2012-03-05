f8624ba6885f7c659619dfb2048736eeae0d93c6
Galileo Galilei <ldv1642@live.com>
2012-03-05 23:33:07 +0000

    Added project files

---
+++ MyDocument.h
@@ -0,0 +1,21 @@
+//
+//  MyDocument.h
+//  Shopping List
+//
+//  Created by jizcakes on 19/02/2012.
+//  Copyright __MyCompanyName__ 2012 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+	IBOutlet NSTableView *shoppingListTableView;
+	IBOutlet NSTextField *newShoppingItemTextField;
+	
+	NSMutableArray *shoppingListArray;
+}
+
+-(IBAction) addNewItem:(id)sender;
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Shopping List
+//
+//  Created by jizcakes on 19/02/2012.
+//  Copyright __MyCompanyName__ 2012 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
